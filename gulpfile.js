71893b326f2e81d1f4014ab92042c1e92a156f67
Estevão Bresolin <estevao.bresolin@wecode.digital>
2023-12-04 15:14:56 -0300

    OUVTEXIO-45 estruturação header

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,69 @@
-const gulp = require('gulp'),
+const 
+ gulp = require('gulp'),
   sass = require('gulp-sass')(require('node-sass')),
-  autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps');
+  //  autoprefixer = require('gulp-autoprefixer'),
+  // postcss = require('gulp-postcss'),
+  sourcemaps = require('gulp-sourcemaps')
 let bases = {
-  src: '.',
-  buildCss: './styles/css',
-  srcReact: './react/components',
-  srcCheckout: './checkout-ui-settings'
-},
+    src: '.',
+    buildCss: './styles/css',
+    srcReact: './react/components',
+    srcCheckout: './checkout-ui-settings',
+  },
   paths = {
     sass: bases.src + '/styles/sass/*.scss',
     sassReact: bases.srcReact + '/**/**.scss',
     sassCheckout: bases.srcCheckout + '/checkout-ui-custom/sass/*.scss',
   }
 gulp.task('sass', () => {
-  return gulp
-    .src([paths.sass], { allowEmpty: true })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest([bases.buildCss]))
+  return (
+    gulp
+      .src([paths.sass], { allowEmpty: true })
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
+      //.pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest([bases.buildCss]))
+  )
 })
 gulp.task('sassReact', () => {
-  return gulp
-    .src([paths.sassReact], {
-      allowEmpty: true,
-    })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(function (file) {
-      return file.base;
-    }));
+  return (
+    gulp
+      .src([paths.sassReact], {
+        allowEmpty: true,
+      })
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
+      //.pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write())
+      .pipe(
+        gulp.dest(function (file) {
+          return file.base
+        })
+      )
+  )
 })
 gulp.task('sassCheckout', () => {
-  return gulp
-    .src([paths.sassCheckout], {
-      allowEmpty: true,
-    })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(function (file) {
-      return file.base;
-    }));
+  return (
+    gulp
+      .src([paths.sassCheckout], {
+        allowEmpty: true,
+      })
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(autoprefixer({cascade: false, overrideBrowserslist:['last  2 versions']}))
+      //.pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write())
+      .pipe(
+        gulp.dest(function (file) {
+          return file.base
+        })
+      )
+  )
 })
 gulp.task('watch', () => {
   gulp.watch(
-    [
-      paths.sass,
-      paths.sassReact,
-      paths.sassCheckout,
-    ],
-    gulp.series(
-      'sass',
-      'sassReact',
-      'sassCheckout',
-    )
+    [paths.sass, paths.sassReact, paths.sassCheckout],
+    gulp.series('sass', 'sassReact', 'sassCheckout')
   )
 })
 gulp.task(
